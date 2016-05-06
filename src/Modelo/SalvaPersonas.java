dfde195fe3c7ae7e38cb2f18177c78303f96f7b8
Luis Enrique <Luis Enrique@LuisEnrique>
2016-05-06 10:08:28 -0600

    Aporte Luis: Factory terminada

---
+++ src/Modelo/SalvaPersonas.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Luis Enrique
+ */
+public class SalvaPersonas {
+    public Persona crearPersona(String cedula, String tipo)
+    {
+        Persona per=null;
+        if(tipo.equals("Migrante"))
+        {
+            per=new Migrante(cedula);
+        }
+        return per;
+    }
+    
+}
